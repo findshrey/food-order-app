931c8dcd29bd1c5c6522fff6da85a6c2edccfefb
shrey9496 <shrey9april@gmail.com>
2021-06-15 19:35:01 +0530

    Feat: add/ remove items from cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,14 @@ import CartContext from '../../context/CartContext'
 const Cart = () => {
    const cartCtx = useContext(CartContext)
 
+   const handleAddItem = (item) => {
+      cartCtx.addItem({ ...item, quantity: 1 })
+   }
+
+   const handleRemoveItem = (id) => {
+      cartCtx.removeItem(id)
+   }
+
    return (
       <section className="cart">
          <div className="container">
@@ -16,7 +24,14 @@ const Cart = () => {
                {
                   cartCtx.cartItems.map((item) => (
                      <li>
-                        {`Item: ${item.name}, Quantity: ${item.quantity}`}
+                        <div>
+                           {`Item: ${item.name}, Price: ${item.price}`}
+                        </div>
+                        <div>
+                           <button onClick={handleAddItem.bind(null, item)}>+</button>
+                           <span>{item.quantity}</span>
+                           <button onClick={handleRemoveItem.bind(null, item.id)}>-</button>
+                        </div>
                      </li>
                   ))
                }

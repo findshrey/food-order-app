2f8e68761317df22ca42395aaaa8d0a6f5c79bee
Shrey <shrey9april@gmail.com>
2021-09-05 23:38:32 +0530

    Feat: signup account

--- src/components/SignupForm/SignUpForm.js
+++ src/components/SignupForm/SignUpForm.js
@@ -5,10 +5,40 @@ const SignUp = ({ handleAuthMode }) => {
    const passwordRef = useRef()
    const confirmPasswordRef = useRef()
 
+   const handleSubmit = (e) => {
+      e.preventDefault()
+
+      // Add validation
+
+      fetch(
+         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAfdliDaU39tcKz0o6mP08DN1Ie0lGmhE",
+         {
+            method: "POST",
+            body: JSON.stringify({
+               email: emailRef.current.value,
+               password: passwordRef.current.value,
+               returnSecureToken: true,
+            }),
+            headers: {
+               "Content-Type": "application/json",
+            },
+         }
+      ).then((res) => {
+         if (res.ok) {
+            // ..
+         } else {
+            return res.json().then((data) => {
+               // show error modal
+               console.log(data)
+            })
+         }
+      })
+   }
+
    return (
       <div className="sign-up">
          <h3>Sign Up</h3>
-         <form>
+         <form onSubmit={handleSubmit}>
             <div className="form-control">
                <label>Email:</label>
                <input type="email" ref={emailRef} required />

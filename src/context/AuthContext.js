e3918d9aeccabeb191d2add71cf17fb116ac2103
Shrey <shrey9april@gmail.com>
2021-09-10 02:45:38 +0530

    Feat: user authentication persistance

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const AuthContext = React.createContext()
 
 const AuthProvider = ({ children }) => {
    const [token, setToken] = useState(null)
 
+   useEffect(() => {
+      const storedToken = localStorage.getItem("token")
+
+      if (storedToken) {
+         setToken(storedToken)
+      }
+   }, [])
+
    // Check if user is logged in
    const userIsLoggedIn = !!token
 
    const handleLogIn = (token) => {
       setToken(token)
+      localStorage.setItem("token", token)
    }
 
    const handleLogOut = () => {
       setToken(null)
+      localStorage.removeItem("token")
    }
 
    // Context data to share

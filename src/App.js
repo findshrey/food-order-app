79eb533da40c253e02e28f46eafac031397822df
Shrey <shrey9april@gmail.com>
2021-09-10 00:11:02 +0530

    Refactor: moved auth provider to outermost node

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
 import React, { useContext } from "react"
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 
 import Header from "./components/Header/Header"
 import Menu from "./pages/Menu/Menu"
 import MenuCategory from "./pages/MenuCategory/MenuCategory"
 import Cart from "./pages/Cart/Cart"
 import NotFound from "./pages/NotFound/NotFound"
-import { CartProvider } from "./context/CartContext"
 import AuthPage from "./pages/AuthPage"
-import AuthContext, { AuthProvider } from "./context/AuthContext"
+import AuthContext from "./context/AuthContext"
 import ProfilePage from "./pages/ProfilePage"
 
 function App() {
    const authCtx = useContext(AuthContext)
 
    return (
-      <AuthProvider>
-         <CartProvider>
-            <BrowserRouter>
-               <Header />
-               <main>
-                  <Switch>
-                     {!authCtx?.isLoggedIn && (
-                        <Route path="/auth">
-                           <AuthPage />
-                        </Route>
-                     )}
-                     <Route path="/profile">
-                        {authCtx?.isLoggedIn ? (
-                           <ProfilePage />
-                        ) : (
-                           <Redirect to="/auth" />
-                        )}
-                     </Route>
-                     <Route path="/menu" exact>
-                        <Menu />
-                     </Route>
-                     <Route path="/menu/:category">
-                        <MenuCategory />
-                     </Route>
-                     <Route path="/cart">
-                        <Cart />
-                     </Route>
-                     <Route path="*">
-                        <NotFound />
-                     </Route>
-                  </Switch>
-               </main>
-            </BrowserRouter>
-         </CartProvider>
-      </AuthProvider>
+      <>
+         <Header />
+         <main>
+            <Switch>
+               {!authCtx.isLoggedIn && (
+                  <Route path="/auth">
+                     <AuthPage />
+                  </Route>
+               )}
+               <Route path="/profile">
+                  {authCtx.isLoggedIn ? (
+                     <ProfilePage />
+                  ) : (
+                     <Redirect to="/auth" />
+                  )}
+               </Route>
+               <Route path="/menu" exact>
+                  <Menu />
+               </Route>
+               <Route path="/menu/:category">
+                  <MenuCategory />
+               </Route>
+               <Route path="/cart">
+                  <Cart />
+               </Route>
+               <Route path="*">
+                  <NotFound />
+               </Route>
+            </Switch>
+         </main>
+      </>
    )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
 
-import './index.scss'
-import App from './App'
+import "./index.scss"
+import App from "./App"
+import { AuthProvider } from "./context/AuthContext"
+import { CartProvider } from "./context/CartContext"
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+   <AuthProvider>
+      <CartProvider>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </CartProvider>
+   </AuthProvider>,
+   document.getElementById("root")
+)

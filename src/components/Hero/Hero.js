05f09f140ebb697976e6ffed2103d65f4b91b1ee
Shrey <shrey9april@gmail.com>
2021-10-02 23:47:21 +0530

    Style: Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,10 @@
 import React from "react"
 
+import styles from "./Hero.module.scss"
+
 const Hero = () => {
    return (
-      <section className="hero">
+      <section className={styles.hero}>
          <p>Hero Page</p>
       </section>
    )

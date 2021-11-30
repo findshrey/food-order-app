90d5406aee2e563a6a8170b0d8fb438c749736b0
Shrey <shrey9april@gmail.com>
2021-11-30 21:45:45 +0530

    Refactor: cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react"
+import { useNavigate } from "react-router-dom"
 
+import * as ROUTES from "../../constants/routes"
 import CartContext from "../../context/CartContext"
 import CartItem from "./CartItem"
 import CartSummary from "./CartSummary"
@@ -10,6 +12,7 @@ import styles from "./Cart.module.scss"
 const Cart = () => {
    const { isLoading, error, sendRequest: placeOrder } = useHttp()
    const cartCtx = useContext(CartContext)
+   let navigate = useNavigate()
 
    const handleAddItem = (item) => {
       cartCtx.addItem({ ...item, quantity: 1 })
@@ -35,29 +38,43 @@ const Cart = () => {
       )
    }
 
+   const hasItems = cartCtx.cartItems.length > 0
+
    return (
       <div className={styles.cart}>
          <header className={styles["cart-head"]}>
             <h2>My Cart</h2>
          </header>
-         <div className={styles["cart-inner"]}>
-            <section className={styles["cart-item-list"]}>
-               <ul>
-                  {cartCtx.cartItems.map((item) => (
-                     <CartItem
-                        item={item}
-                        handleAddItem={handleAddItem}
-                        handleRemoveItem={handleRemoveItem}
-                     />
-                  ))}
-               </ul>
-            </section>
-            <CartSummary
-               items={cartCtx.cartItems}
-               totalAmount={cartCtx.totalAmount}
-               handleOrder={handleOrder}
-            />
-         </div>
+         {!hasItems ? (
+            <div className={styles["empty-cart"]}>
+               <p>
+                  YOUR CART IS EMPTY. <br />
+                  LET'S START AN ORDER!
+               </p>
+               <button onClick={() => navigate(ROUTES.MENU)}>
+                  Start Order
+               </button>
+            </div>
+         ) : (
+            <div className={styles["cart-inner"]}>
+               <section className={styles["cart-item-list"]}>
+                  <ul>
+                     {cartCtx.cartItems.map((item) => (
+                        <CartItem
+                           item={item}
+                           handleAddItem={handleAddItem}
+                           handleRemoveItem={handleRemoveItem}
+                        />
+                     ))}
+                  </ul>
+               </section>
+               <CartSummary
+                  items={cartCtx.cartItems}
+                  totalAmount={cartCtx.totalAmount}
+                  handleOrder={handleOrder}
+               />
+            </div>
+         )}
       </div>
    )
 }

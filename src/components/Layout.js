81cfce202fa6d0afacc8fa59de2eca7137961ed4
Shrey <shrey9april@gmail.com>
2021-12-02 23:25:47 +0530

    Fix: white space below main content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,15 @@ import React from "react"
 import Footer from "./Footer/Footer"
 import Header from "./Header/Header"
 
+import styles from "./Layout.module.scss"
+
 const Layout = (props) => {
    return (
-      <>
+      <div className={styles.layout}>
          <Header />
-         <main className="main-content">{props.children}</main>
+         <main className={styles["main-content"]}>{props.children}</main>
          <Footer />
-      </>
+      </div>
    )
 }
 

12f0bc4483e82bf117d68b00cbc9bc77a89c0c1c
Shrey <shrey9april@gmail.com>
2021-12-12 23:32:16 +0530

    Style: change password form

--- src/components/Profile/ChangePassword.js
+++ src/components/Profile/ChangePassword.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react"
 
 import useHttp from "../../hooks/useHttp"
 
+import styles from "./ChangePassword.module.scss"
+
 const ChangePassword = ({ token }) => {
    const { isLoading, error, sendRequest: updatePassword } = useHttp()
    const passwordRef = useRef()
@@ -37,20 +39,29 @@ const ChangePassword = ({ token }) => {
    }
 
    return (
-      <section className="change-password">
+      <section className={styles["change-password"]}>
          <header>
             <h3>Change Your Password</h3>
+            <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+               sit doloremque facere itaque mollitia officia laudantium
+               cupiditate nemo ea eligendi.
+            </p>
          </header>
-         <form onSubmit={handlePassword}>
-            <div className="form-control">
-               <label>New Password</label>
-               <input type="password" ref={passwordRef} required />
+         <form className={styles["change-pass-form"]} onSubmit={handlePassword}>
+            <div className={styles["fields-wrapper"]}>
+               <div className={styles["form-control"]}>
+                  <label>New Password</label>
+                  <input type="password" ref={passwordRef} required />
+               </div>
+               <div className={styles["form-control"]}>
+                  <label>Confirm Password</label>
+                  <input type="password" ref={passwordConfirmRef} required />
+               </div>
             </div>
-            <div className="form-control">
-               <label>Confirm Password</label>
-               <input type="password" ref={passwordConfirmRef} required />
+            <div className={styles["btn-wrapper"]}>
+               <button type="submit">Change</button>
             </div>
-            <button type="submit">Change</button>
          </form>
       </section>
    )

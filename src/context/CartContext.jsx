e5f9785fb612ae263c5ca0ef75851774149e7ebd
findshrey <findshrey@outlook.com>
2025-05-15 15:54:19 +0530

    Migrate: from CRA to VITE

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,117 @@
+import React, { useReducer } from "react"
+
+const ACTIONS = {
+   ADD_ITEM: "add-item",
+   REMOVE_ITEM: "remove-item",
+}
+
+const CartContext = React.createContext({
+   items: [],
+   totalAmount: 0,
+   addItem: (item) => {},
+   removeItem: (id) => {},
+})
+
+// useReducer default state
+const defaultCartState = {
+   items: [],
+   totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+   switch (action.type) {
+      case ACTIONS.ADD_ITEM: {
+         const updatedAmount =
+            state.totalAmount + action.item.quantity * action.item.price
+
+         // Check if cart item already exists
+         const existingItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+         )
+         const existingItem = state.items[existingItemIndex]
+
+         let updatedItems
+
+         if (existingItem) {
+            const updateExistingItem = {
+               ...existingItem,
+               quantity: existingItem.quantity + 1,
+            }
+
+            updatedItems = [...state.items]
+            updatedItems[existingItemIndex] = updateExistingItem
+         } else {
+            updatedItems = [...state.items, action.item]
+         }
+
+         return {
+            items: updatedItems,
+            totalAmount: updatedAmount,
+         }
+      }
+
+      case ACTIONS.REMOVE_ITEM: {
+         // Check if cart item already exists
+         const existingItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+         )
+         const existingItem = state.items[existingItemIndex]
+
+         const updatedAmount = state.totalAmount - existingItem.price
+
+         let updatedItems
+
+         if (existingItem.quantity === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id)
+         } else {
+            const updateExistingItem = {
+               ...existingItem,
+               quantity: existingItem.quantity - 1,
+            }
+
+            updatedItems = [...state.items]
+            updatedItems[existingItemIndex] = updateExistingItem
+         }
+
+         return {
+            items: updatedItems,
+            totalAmount: updatedAmount,
+         }
+      }
+
+      default: {
+         return defaultCartState
+      }
+   }
+}
+
+const CartProvider = ({ children }) => {
+   const [cartState, dispatchCartAction] = useReducer(
+      cartReducer,
+      defaultCartState
+   )
+
+   const handleAddItem = (item) => {
+      dispatchCartAction({ type: ACTIONS.ADD_ITEM, item: item })
+   }
+
+   const handleRemoveItem = (id) => {
+      dispatchCartAction({ type: ACTIONS.REMOVE_ITEM, id: id })
+   }
+
+   // Context data to share
+   const contextData = {
+      cartItems: cartState.items,
+      totalAmount: cartState.totalAmount,
+      addItem: handleAddItem,
+      removeItem: handleRemoveItem,
+   }
+
+   return (
+      <CartContext.Provider value={contextData}>
+         {children}
+      </CartContext.Provider>
+   )
+}
+
+export { CartContext as default, CartProvider }

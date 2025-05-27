fa48fb603c9ebcc120066c378d07e9408083b41e
findshrey <findshrey@outlook.com>
2025-05-27 15:03:51 +0530

    feat: added toast notification

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from "react-toastify"
+
 import Footer from "./Footer"
 import Header from "./Header"
 
@@ -6,6 +8,7 @@ import styles from "./Layout.module.scss"
 const Layout = (props) => {
    return (
       <div className={styles.layout}>
+         <ToastContainer theme="dark" />
          <Header />
          <main className={styles["main-content"]}>{props.children}</main>
          <Footer />

cf1435bbe965df9a107e22dddb785c3f66290077
findshrey <findshrey@outlook.com>
2025-05-27 14:19:23 +0530

    feat: added validations for sign-up form

--- src/components/Forms/SignUpForm.jsx
+++ src/components/Forms/SignUpForm.jsx
@@ -7,15 +7,77 @@ import useHttp from "../../hooks/useHttp"
 
 import styles from "./Forms.module.scss"
 
+const validationCheck = (data) => {
+   let isValid = true
+   const errors = {}
+
+   const { email, password, phone, address } = data
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+   // indian mobile number - should start with 6, 7, 8, or 9 and be 10 digits long
+   const mobileRegex = /^[6-9]\d{9}$/
+
+   // Utility to check if a value is empty (handles strings, null, undefined)
+   const isEmpty = (val) => !val || val.trim() === ""
+
+   if (isEmpty(email)) {
+      errors.email = "Email is required."
+      isValid = false
+   } else if (!emailRegex.test(email)) {
+      errors.email = "Please enter a valid email address."
+      isValid = false
+   }
+
+   if (isEmpty(password)) {
+      errors.password = "Password is required."
+      isValid = false
+   } else if (password.length < 6) {
+      errors.password = "Password must be at least 6 characters long."
+      isValid = false
+   }
+
+   if (isEmpty(phone)) {
+      errors.phone = "Phone number is required."
+      isValid = false
+   } else if (!mobileRegex.test(phone)) {
+      errors.phone = "Please enter a valid mobile number."
+      isValid = false
+   }
+
+   if (isEmpty(address)) {
+      errors.address = "Address is required."
+      isValid = false
+   }
+
+   return { isValid, errors }
+}
+
 const SignUpForm = ({ handleFormMode }) => {
    const [passVisible, setPassVisible] = useState(false)
    const { isLoading, error, sendRequest: signUpRequest } = useHttp()
    const authCtx = useContext(AuthContext)
 
-   const emailRef = useRef()
-   const passwordRef = useRef()
-   const phoneRef = useRef()
-   const addressRef = useRef()
+   const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+      phone: "",
+      address: "",
+   })
+
+   const [formValidations, setFormValidations] = useState({
+      email: "",
+      password: "",
+      phone: "",
+      address: "",
+   })
+
+   const handleFormDataChange = (e) => {
+      const { name, value } = e.target
+
+      setFormData((prevData) => ({
+         ...prevData,
+         [name]: value,
+      }))
+   }
 
    const navigate = useNavigate()
 
@@ -27,6 +89,20 @@ const SignUpForm = ({ handleFormMode }) => {
    const handleSignUp = (e) => {
       e.preventDefault()
 
+      const { isValid, errors } = validationCheck(formData)
+
+      if (!isValid) {
+         setFormValidations(errors)
+         return
+      } else {
+         setFormValidations({
+            email: "",
+            password: "",
+            phone: "",
+            address: "",
+         })
+      }
+
       signUpRequest(
          {
             url: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAfdliDaU39tcKz0o6mP08DN1Ie0lGmhE",
@@ -35,8 +111,8 @@ const SignUpForm = ({ handleFormMode }) => {
                "Content-Type": "application/json",
             },
             body: {
-               email: emailRef.current.value,
-               password: passwordRef.current.value,
+               email: formData.email,
+               password: formData.password,
                returnSecureToken: true,
             },
          },
@@ -50,9 +126,9 @@ const SignUpForm = ({ handleFormMode }) => {
                      "Content-Type": "application/json",
                   },
                   body: JSON.stringify({
-                     name: emailRef.current.value.split("@")[0],
-                     phone: phoneRef.current.value,
-                     address: addressRef.current.value,
+                     name: formData.email.split("@")[0],
+                     phone: formData.phone,
+                     address: formData.address,
                   }),
                }
             )
@@ -73,23 +149,51 @@ const SignUpForm = ({ handleFormMode }) => {
          <form onSubmit={handleSignUp}>
             <div className={styles["form-control"]}>
                <label>Email:</label>
-               <input type="email" ref={emailRef} required />
+               <input
+                  type="text"
+                  name="email"
+                  onChange={handleFormDataChange}
+                  value={formData.email}
+               />
+               <p className={styles["validation-err"]}>
+                  {formValidations.email}
+               </p>
             </div>
             <div className={styles["form-control"]}>
                <label>Password:</label>
                <input
                   type={passVisible ? "text" : "password"}
-                  ref={passwordRef}
-                  required
+                  name="password"
+                  onChange={handleFormDataChange}
+                  value={formData.password}
                />
+               <p className={styles["validation-err"]}>
+                  {formValidations.password}
+               </p>
             </div>
             <div className={styles["form-control"]}>
                <label>Phone:</label>
-               <input type="number" ref={phoneRef} required />
+               <input
+                  type="number"
+                  name="phone"
+                  onChange={handleFormDataChange}
+                  value={formData.phone}
+               />
+               <p className={styles["validation-err"]}>
+                  {formValidations.phone}
+               </p>
             </div>
             <div className={styles["form-control"]}>
                <label>Address:</label>
-               <input type="text" ref={addressRef} required />
+               <input
+                  type="text"
+                  name="address"
+                  onChange={handleFormDataChange}
+                  value={formData.address}
+               />
+               <p className={styles["validation-err"]}>
+                  {formValidations.address}
+               </p>
             </div>
             <div className={styles["form-options"]}>
                <input
